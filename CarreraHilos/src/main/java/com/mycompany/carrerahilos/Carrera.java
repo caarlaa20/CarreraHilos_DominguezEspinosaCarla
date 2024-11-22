0974808f830ee8bf234a84ed37426052950f3e48
Carla domínguez <carladominguez504@gmail.com>
2024-11-22 18:52:17 +0100

    creao nuestra clase carrera

---
+++ CarreraHilos/src/main/java/com/mycompany/carrerahilos/Carrera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carrerahilos;
+
+/**
+ *
+ * @author alumno
+ */
+public class Carrera {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
